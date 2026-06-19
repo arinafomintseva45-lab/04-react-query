7fcb368616c56da8f04092c4e6ebe4d35a2e794c
arinafomintseva45-lab <arinafomintseva45@gmail.com>
2026-06-19 18:46:15 +0200

    poprawa

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./components/App/App";
 
-import "modern-normalize";
+import "./styles/global.css";
 import "./index.css";
 
 const queryClient = new QueryClient();
